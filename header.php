b94b78a9a809bcdb55f82c031ccdcb09f30a001b
HansGammeltoft <65598393+HansGammeltoft@users.noreply.github.com>
2021-05-18 09:10:16 +0200

    Nav er blevet lavet til mobil version
    Der nav er lavet til mobil version og den er næsten færdig til desktop versionen også.
    
    Det er dog nogle enkelte ting der skal laves i CSS'en, og potentielt skal js i headeren eksekveres med CSS i en nyere version.

--- header.php
+++ header.php
@@ -4,11 +4,12 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <script src="https://kit.fontawesome.com/9068d98231.js" crossorigin="anonymous"></script>
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
     <title></title>
     <?php wp_head();?>
   </head>
   <body <?php body_class('');?>>
-    <div class="col-sm-12 container d-flex align-items-center justify-content-around">
+    <div class="col-sm-12 container d-flex  justify-content-around">
       <p>USP text</p>
       <p>USP text</p>
       <p>USP text</p>
@@ -16,36 +17,46 @@
       <p>USP text</p>
       <p>USP text</p>
     </div>
-    <header class="sticky-top col-sm-12">
-      <a href="javascript:void(0);" class="icon d-block d-md-none col-sm-2" onclick="myFunction()">
-        <i class="fa fa-bars"></i>
-      </a>
-      <nav class="col-sm-10 col-md-12 col-lg-12 container d-flex">
-        <a class="col-sm-10 col-md-2 col-lg-1" href="/larsenshundeartikler">
-          <img class="img-responsive logo" src="/larsenshundeartikler/wp-content/uploads/2021/05/logo-header.png" alt="">
+    <header class="sticky-top  container d-flex col-12 col-sm-12 col-md-12 col-lg-12 d-flex">
+
+      <div class="icon col-4 col-sm-4 d-md-none  d-flex justify-content-center">
+        <a href="javascript:void(0);" class="col-6 col-sm-6 d-block d-md-none  d-flex justify-content-center" onclick="myFunction()">
+          <i class="fa fa-bars"></i>
         </a>
-        <div id="myLinks" class="navmenu col-sm-12 col-sm-9 col-lg-10 align-items-center">
-          <div class="mobile-searchbar col-sm-5 col-md-5 col-lg-3" >
+        <a href="javascript:void(0);" class="col-6 col-sm-6 d-block d-md-none  d-flex justify-content-center" onclick="myFunction()">
+          <i class="fas fa-search"></i>
+        </a>
+      </div>
+
+      <a class="logo-wrapper col-4 col-sm-4 col-md-2 col-lg-2" href="/larsenshundeartikler">
+        <img class="img-responsive logo" src="/larsenshundeartikler/wp-content/uploads/2021/05/logo-header.png" alt="">
+      </a>
+
+      <nav id="myLinks" class="col-12 col-sm-12 col-md-9 col-lg-9 container">
+        <div class="navmenu ">
+          <div class="mobile-searchbar" >
             <?php echo do_shortcode('[fibosearch]'); ?>
           </div>
-          <div class="top_menu col-sm-7 col-md-7 col-lg-9">
+          <div class="top_menu">
             <?php
               wp_nav_menu(
                 array(
-                  'theme_location' => 'top-menu'
+                  'theme_location' => 'top-menu',
+                  //'after' => '<a class="link-arrowdown" href="?op=1"><p class="arrowdown">&#9660;</p><a>'
                 )
               );
             ?>
           </div>
-          <div class="desktop-searchbar col-sm-5 col-md-5 col-lg-3" >
+          <div class="desktop-searchbar" >
             <?php echo do_shortcode('[fibosearch]'); ?>
           </div>
         </div>
-        <div class="col-sm-2 col-md-1 col-lg-1 wishlist-cart d-flex align-items-center justify-content-around">
-          <a href="#"><i class="fas fa-heart"></i></a>
-          <a href="/larsenshundeartikler/cart/"><i class="fas fa-shopping-cart"></i></a>
-        </div>
       </nav>
+
+      <div class="wishlist-cart col-4 col-sm-4 col-md-1 col-lg-1 d-flex  d-md-flex d-flex justify-content-around">
+        <a href="#"><i class="fas fa-heart"></i></a>
+        <a href="/larsenshundeartikler/cart/"><i class="fas fa-shopping-cart"></i></a>
+      </div>
     </header>
     <?php if (class_exists('WooCommerce') && is_woocommerce()) : ?>
 
@@ -56,10 +67,23 @@
 <script>
 function myFunction() {
   var x = document.getElementById("myLinks");
-  if (x.style.display === "block") {
+  if (x.style.display === "flex") {
     x.style.display = "none";
   } else {
-    x.style.display = "block";
+    x.style.display = "flex";
   }
 }
+
+
+if (jQuery(window).width() < 768) {
+  $(function(){
+      $(".sub-menu").hide();
+      $(".sub-menu-link").click(function(e){
+          e.preventDefault();
+          $(".sub-menu", $(this).parent()).slideToggle();
+      });
+  })
+}
+
+
 </script>
